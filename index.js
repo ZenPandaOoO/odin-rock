4b0cab3359d274b860b050f0a155991eeeec36d1
ZenPandaOoO <hubbaandmilitaries@gmail.com>
2025-03-04 20:43:24 -0500

    Created html and script. Computer Choice started.

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log ("Hello World");
+
+
+function getComputerChoice() {
+    let rock = "Rock";
+    let paper = "Paper";
+    let scissors = "Scissors";
+    let getRandomValue = Math.random();
+        //if value is less or equal to .33 then rock
+        //if value is less then or equal to .66 then paper
+        //if value is less than or equal to .99 then scissors
+
+}
+
+console.log (getComputerChoice);
+
+//Create a function named getComputerChoice
+    //Randomly selects Rock, Paper, or Scissors w/ Math.random
+
+
+//Create a function named getHumanChoice
+    //Use prompt method to get user's input
