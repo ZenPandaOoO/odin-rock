8f5676153a2fc05ecdf48ef57f0e065c080a6fe1
ZenPandaOoO <hubbaandmilitaries@gmail.com>
2025-03-07 22:55:17 -0500

    getComputerChoice pulls random value of rock, paper, or scissors. Need to start function for getHumanChoice.

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-console.log ("Hello World");
-
-
 function getComputerChoice() {
     let rock = "Rock";
     let paper = "Paper";
     let scissors = "Scissors";
     let getRandomValue = Math.random();
-        //if value is less or equal to .33 then rock
-        //if value is less then or equal to .66 then paper
-        //if value is less than or equal to .99 then scissors
+
+    if (getRandomValue <= 0.33) {
+        return rock;
+    } else if (getRandomValue <= 0.66) {
+        return paper;
+    } else {
+        return scissors;
+        }
 
 }
-
-console.log (getComputerChoice);
+console.log(getComputerChoice());
 
 //Create a function named getComputerChoice
-    //Randomly selects Rock, Paper, or Scissors w/ Math.random
-
+        //if value is less or equal to .33 then rock
+        //if value is less then or equal to .66 then paper
+        //else then scissors
 
 //Create a function named getHumanChoice
     //Use prompt method to get user's input
