189b598edf879947774a75c743b78ab5e0734cb1
ZenPandaOoO <hubbaandmilitaries@gmail.com>
2025-03-13 18:29:33 -0400

    getComputerChoice and getHumanChoice seem to store the correct variable in the functions. playRound stops at Tie.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+    const rock = "Rock";
+    const paper = "Paper";
+    const scissors = "Scissors";
+
 function getComputerChoice() {
-    let rock = "Rock";
-    let paper = "Paper";
-    let scissors = "Scissors";
+
     let getRandomValue = Math.random();
 
     if (getRandomValue <= 0.33) {
@@ -20,5 +22,49 @@ console.log(getComputerChoice());
         //if value is less then or equal to .66 then paper
         //else then scissors
 
-//Create a function named getHumanChoice
-    //Use prompt method to get user's input
+function getHumanChoice() {
+    let humanChoice = prompt("ROCK PAPER SCISSORS");
+    if (humanChoice.toLowerCase() === "rock") {
+        return rock;
+    } else if (humanChoice.toLowerCase() === "paper") {
+        return paper;
+    } else if (humanChoice.toLowerCase() === "scissors") {
+        return scissors;
+    } else {
+        return "Not an option.";
+        }
+}
+console.log (getHumanChoice());
+
+
+
+function playRound(getHumanChoice, getComputerChoice) {
+
+    let Tie = "Its a tie. Great minds think alike.";
+    let roundWin = `You win! ${getHumanChoice} beats ${getComputerChoice}`;
+    let roundLose = `You lose. ${getComputerChoice} beats ${getHumanChoice}`;
+    
+    if (getHumanChoice = getComputerChoice) {
+        return Tie;
+    } else if ((getHumanChoice == "Rock") && (getComputerChoice == "Scissors")) {
+        return roundWin;  
+    } else if ((getHumanChoice == "Paper") && (getComputerChoice == "Rock")) {
+        return roundWin;
+    } else if ((getHumanChoice == "Scissors") && (getComputerChoice == "Paper")) {
+        return roundWin;
+    } else {
+        return roundLose;
+    }
+}
+
+console.log (playRound(getHumanChoice, getComputerChoice));
+
+
+
+
+//Create a function named checkWinner
+    //if getHumanChoice = getComputerChoice return tie
+    // else if HumanChoice is rock and computerChoice is scissors return win
+    // else if HumanChoice is paper and computerChoice is rock return win
+    // else if HumanChoice is scissors and computerChoice is paper return win
+    // else return lose
