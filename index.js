c318f83ebca90ef11edb9be9cdae04a54b3aa83d
ZenPandaOoO <hubbaandmilitaries@gmail.com>
2025-03-20 12:43:26 -0400

    Added checkWinner and modified human selection. No longer does multiple prompt when calling playRound.

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
+function getComputerChoice() {
     const rock = "Rock";
     const paper = "Paper";
     const scissors = "Scissors";
-
-function getComputerChoice() {
-
     let getRandomValue = Math.random();
 
     if (getRandomValue <= 0.33) {
@@ -16,62 +14,47 @@ function getComputerChoice() {
 
 }
 
-
-//Create a function named getComputerChoice
-        //if value is less or equal to .33 then rock
-        //if value is less then or equal to .66 then paper
-        //else then scissors
-
-    let options = prompt("ROCK PAPER SCISSORS");
-    const humanChoiceLower = options.toLowerCase();
-
-function getHumanChoice() {
-
-    if (humanChoiceLower === "rock") {
-        return rock;
-    } else if (humanChoiceLower === "paper") {
-        return paper;
-    } else if (humanChoiceLower === "scissors") {
-        return scissors;
-    } else {
-        return "Mayo";
-        }
+function playerChoice() {
+    let options = prompt("TYPE ROCK PAPER OR SCISSORS!");
+    let playerChoiceLower = options.toLowerCase();
+    return playerChoiceLower.charAt(0).toUpperCase(1) + playerChoiceLower.slice(1);
 }
 
-
-
-function playRound(humanChoice, computerChoice) {
-
+function checkWinner(ChoiceP, ChoiceC) {
     let Tie = "Its a tie. Great minds think alike.";
     let roundWin = `You win! ${humanSelection} beats ${computerSelection}`;
     let roundLose = `You lose. ${computerSelection} beats ${humanSelection}`;
-    
-    if (humanSelection === computerSelection) {
+
+        if (ChoiceP === computerSelection) {
         return Tie;
-    } else if ((humanSelection == "Rock") && (computerSelection == "Scissors")) {
+    } else if ((ChoiceP == "Rock") && (ChoiceC == "Scissors")) {
         return roundWin;  
-    } else if ((humanSelection == "Paper") && (computerSelection == "Rock")) {
+    } else if ((ChoiceP == "Paper") && (ChoiceC == "Rock")) {
         return roundWin;
-    } else if ((humanSelection == "cissors") && (computerSelection == "Paper")) {
+    } else if ((ChoiceP == "Scissors") && (ChoiceC == "Paper")) {
         return roundWin;
     } else {
         return roundLose;
-    }
+}
 }
 
-const humanSelection = getHumanChoice();
+function playRound() {
+    const playerSelection = playerChoice();
+    const computerResult = getComputerChoice();
+    const winner = checkWinner(humanSelection, computerSelection);
+    //if winner = roundWin player gets +1
+    //else if winner = roundLose computer gets +1
+    //else nobody gets a point
+}
+
+const humanSelection = playerChoice();
 const computerSelection = getComputerChoice();
 
 console.log(computerSelection);
-console.log (humanSelection);
-console.log (playRound(humanSelection, computerSelection));
 
+console.log (checkWinner(humanSelection, computerSelection));
 
-
+//create new functions called playGame
 
 //Create a function named checkWinner
-    //if getHumanChoice = getComputerChoice return tie
-    // else if HumanChoice is rock and computerChoice is scissors return win
-    // else if HumanChoice is paper and computerChoice is rock return win
-    // else if HumanChoice is scissors and computerChoice is paper return win
-    // else return lose
+   
