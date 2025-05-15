acab4c5ee5783b923f926ecd65d5ddaeecea04d4
ZenPandaOoO <hubbaandmilitaries@gmail.com>
2025-05-15 18:57:53 -0400

    Reverse computer and player sections to show score before names. turned playerChoice() into a var to interact with buttons easier.

--- index.js
+++ index.js
@@ -16,13 +16,20 @@ function getComputerChoice() {
 
 }
 
-//prompt for player choice. Returns as first letter capitalized.
-function playerChoice() {
-    let options = prompt("TYPE ROCK PAPER OR SCISSORS!");
-    let playerChoiceLower = options.toLowerCase();
-    return playerChoiceLower.charAt(0).toUpperCase(1) + playerChoiceLower.slice(1);
-}
+let playerChoice
 
+//buttons
+const rockButton = document.querySelector("#rock");
+rockButton.addEventListener("click", () => {
+    let playerChoice = "Rock"
+});
+
+//prompt for player choice. Returns as first letter capitalized.
+
+
+
+
+    
 //compares player choice and computer choice. Determines the winner.
 function checkWinner(ChoiceP, ChoiceC) {
     let Tie = "Tie!";
@@ -80,8 +87,8 @@ function playGame() {
 const header = document.querySelector("#head");
 const headContent = document.createElement("div");
 headContent.classList.add("headContent");
-headContent.textContent = "Rock Paper Scissors -- WAR";
-headContent.setAttribute("style", "color: red; background: black; text-align: center; font-weight: bold; font-size: 50px; padding: 24px; margin-top: 75px;");
+headContent.textContent = "War in Heaven";
+headContent.setAttribute("style", "color: GreenYellow; background: black; text-align: center; font-weight: bold; font-size: 50px; padding: 24px; margin-top: 40px;");
 header.appendChild(headContent);
 
 //css for the hero section
@@ -92,6 +99,14 @@ playerContent.textContent = "HERO";
 playerContent.setAttribute("style", "color: gold; border: solid 8px; border-color: gold; font-size: 40px; padding: 24px; text-align: center;");
 playerSection.appendChild(playerContent);
 
+//create a new div class for hero score inside of div id playerSection
+const playerScore = document.createElement("div");
+playerScore.textContent = "0";
+playerScore.setAttribute("style", "color: gold; border: solid 8px; margin-right: 12px; border-color: gold; font-size: 40px; padding: 24px; text-align: center;");
+playerSection.appendChild(playerScore);
+
+//create a new div class for hero choice
+
 //css for the villain section
 const computerSection = document.querySelector("#computerSection");
 const computerContent = document.createElement("div")
@@ -99,6 +114,18 @@ computerContent.classList.add("computerContent");
 computerContent.textContent = "VILLAIN";
 computerContent.setAttribute("style", "color: purple; background-color: black; border: solid 8px; border-color: purple; font-size: 40px; padding: 20px; text-align: center;");
 computerSection.appendChild(computerContent);
+
+//create a new div class for villain score inside of div id villain section
+const computerScore = document.createElement("div");
+computerScore.textContent = "0";
+computerScore.setAttribute("style", "color: purple; margin-right: 12px; background-color: black; border: solid 8px; border-color: purple; font-size: 40px; padding: 20px; text-align: center;");
+computerSection.appendChild(computerScore);
+
+
+//create a new div class for villain choice
+
+//create a new div class for winner
+
 
 //ODIN ASSIGNMENT//
 //remove logic that plays 5 rounds
